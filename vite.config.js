ffa6266390ade42449bb8e1f5e076527cc31048f
Srihitha74 <143410236+Srihitha705@users.noreply.github.com>
2025-08-04 02:45:09 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/cicd_1/', // 👈 matches your GitHub repo name
+});
