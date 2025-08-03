04d78d7ef7dbd075d753ca972869a0113c2af9a2
Srihitha74 <143410236+Srihitha705@users.noreply.github.com>
2025-08-04 03:19:05 +0530

    🛠️ Fix: Use Node.js v20 for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cicd_1/', // exact match to repo name
+  base: '/cicd_1/', // 👈 Important for GitHub Pages
   plugins: [react()],
 })
-
