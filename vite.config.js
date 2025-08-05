ccdaa693a09793320173dc4df15d4d7bcbeb0f93
Srihitha74 <143410236+Srihitha705@users.noreply.github.com>
2025-08-05 11:28:04 +0530

    ✅ Setup CI/CD workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cicd_1/', // 👈 Important for GitHub Pages
   plugins: [react()],
+  base: '/cicd_1/', // 👈 important!
 })
